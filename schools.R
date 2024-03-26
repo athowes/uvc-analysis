ca61dfdfa0e010558fd965bf87b9553d9ba24552
athowes <adamthowes@gmail.com>
2024-03-26 15:27:25 +0000

    School size distribution

--- schools.R
+++ schools.R
@@ -1,1 +1,8 @@
-# https://explore-education-statistics.service.gov.uk/find-statistics/school-pupils-and-their-characteristics/2022-23
+data <- readr::read_csv("data/spc_school_level_underlying_data_23112023.csv")
+
+library(ggplot2)
+
+ggplot(data, aes(x = `headcount of pupils`)) +
+  geom_histogram(bins = 40, col = "grey10", fill = "grey60") +
+  theme_minimal() +
+  labs(x = "Pupil headcount", y = "", caption = "Data from UK School Census 2022/23")
