55cfb0ec5ef86f4a5947720ebe81d16b2806d0cd
athowes <adamthowes@gmail.com>
2024-03-27 11:06:32 +0000

    Simple lognormal fit to the schools data with parameter estimates

--- schools.R
+++ schools.R
@@ -1,9 +1,10 @@
 data <- readr::read_csv("data/spc_school_level_underlying_data_23112023.csv")
 
 library(ggplot2)
+library(dplyr)
 
 ggplot(data, aes(x = `headcount of pupils`)) +
-  geom_histogram(bins = 40, col = "grey10", fill = "grey60") +
+  geom_histogram(bins = 40, col = "grey10", fill = "grey80") +
   theme_minimal() +
   labs(
     title = "Distribution of school sizes in England",
@@ -11,3 +12,25 @@ ggplot(data, aes(x = `headcount of pupils`)) +
     y = "",
     caption = "Data from England School Census 2022/23"
   )
+
+x <- data |>
+  filter(`headcount of pupils` > 0) |>
+  pull(`headcount of pupils`) |>
+  as.numeric()
+
+log_normal_fit <- MASS::fitdistr(x, "log-normal")
+
+ggplot(data, aes(x = `headcount of pupils`)) +
+  geom_histogram(bins = 40, col = "grey10", fill = "grey80", aes(y = ..density..)) +
+  geom_function(fun = dlnorm, args = list(meanlog = log_normal_fit$estimate[["meanlog"]], sdlog = log_normal_fit$estimate[["sdlog"]]), colour = "darkorange3") +
+  theme_minimal() +
+  labs(
+    title = "Distribution of school sizes in England",
+    x = "Pupil headcount",
+    y = "",
+    caption = "Data from England School Census 2022/23"
+  )
+
+log_normal_fit$estimate
+# meanlog    sdlog 
+# 5.488122 1.020391
